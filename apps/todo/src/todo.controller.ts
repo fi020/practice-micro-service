9677b7a550045e5ff9e51717c086d9cbf45ec691
fi020 <forinternship.020@gmail.com>
2025-02-17 17:12:08 +0530

    after adding TodoServiceProtoController in the impliments and then : hover on TodoController class and quick fix it imports the code

--- apps/todo/src/todo.controller.ts
+++ apps/todo/src/todo.controller.ts
@@ -1,9 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { EmptyProto, PostTodoDTOProto, TodoProto, TodoServiceProtoController, TodosProto } from 'proto/todo';
+import { Observable } from 'rxjs';
 
 @Controller()
-export class TodoController {
+export class TodoController implements TodoServiceProtoController {
   constructor(private readonly todoService: TodoService) {}
+  postTodoProto(request: PostTodoDTOProto): Promise<TodoProto> | Observable<TodoProto> | TodoProto {
+    throw new Error('Method not implemented.');
+  }
+  getTodoProto(request: EmptyProto): Promise<TodosProto> | Observable<TodosProto> | TodosProto {
+    throw new Error('Method not implemented.');
+  }
 
   @Get()
   getHello(): string {
