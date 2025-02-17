24fd3b2653ecb63814f0c6087ff82c7e7565dea5
fi020 <forinternship.020@gmail.com>
2025-02-17 17:45:37 +0530

    running at this point shwos : Cannot find module 'webpack'

--- apps/todo/src/todo.controller.ts
+++ apps/todo/src/todo.controller.ts
@@ -5,12 +5,12 @@ import { Observable } from 'rxjs';
 
 @Controller()
 export class TodoController implements TodoServiceProtoController {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: TodoService) { }
   postTodoProto(request: PostTodoDTOProto): Promise<TodoProto> | Observable<TodoProto> | TodoProto {
     throw new Error('Method not implemented.');
   }
   getTodoProto(request: EmptyProto): Promise<TodosProto> | Observable<TodosProto> | TodosProto {
-    throw new Error('Method not implemented.');
+    return this.todoService.getTodos()
   }
 
   @Get()
--- apps/todo/src/todo.service.ts
+++ apps/todo/src/todo.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { TodoProto, TodosProto } from 'proto/todo';
 
 @Injectable()
 export class TodoService {
+  private todos: TodoProto[]=[
+    {
+      idP:1,
+      descriptionP:"oggy",
+      isDoneP:false
+    },
+    {
+      idP:1,
+      descriptionP:"oggy",
+      isDoneP:false
+    }
+  ];
+  getTodos():TodosProto{
+    return {TodoP: this.todos}
+  }
   getHello(): string {
     return 'Hello World!';
   }
