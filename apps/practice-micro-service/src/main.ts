6c6951dc3d4c06441a22fa0bfaf7e85e8bc4c23c
fi020 <forinternship.020@gmail.com>
2025-02-19 14:18:12 +0530

    feat: enable CORS support and add startup log for main server

--- apps/practice-micro-service/src/main.ts
+++ apps/practice-micro-service/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cors from 'cors'; // Import CORS
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Main server running on port debug consleosd `);
+
 }
 bootstrap();
